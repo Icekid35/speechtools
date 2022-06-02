b780b9c0b917cc95eee28aeeeb8142f0d21a5ab8
Bello Habeeb <81009270+Icekid35@users.noreply.github.com>
2022-06-02 01:24:25 -0600

    Update image-to-text.js

--- image-to-text.js
+++ image-to-text.js
@@ -7,16 +7,17 @@ var val
 var imageref
 var imagesrc
 var present=0
-  function getres(fileurl){
+  function getres(file){
+     var firstform=new FormData()
+
+  firstform.append('file',file)
             val={status:'extracting'}
             render()
             fetch('/api',{
             method:'POST',
-            body:JSON.stringify({file:fileurl}),
+            body:firstform,
       headers: {
-        'Content-Type': 'application/json',
-        'mode':'no-cors',
-        'Access-Control-Allow-Origin': '*'
+
         
             }
               
@@ -89,24 +90,8 @@ render()
 else{
      val={status:'uploading file'}
 render()
-  var firstform=new FormData()
-  firstform.append('key','6d207e02198a847aa98d0a2a901485a5')
-  firstform.append('action','upload')
-  firstform.append('source',file)
-  fetch('https://freeimage.host/api/1/upload',{
-    method:'POST',
-    body:firstform,
-    headers:{
-    'Content-Type': 'application/json',
-        'mode':'no-cors',
-        'Access-Control-Allow-Origin': '*'
-
-    }
-  }).then(res=>res.json()).then(res=>{
-    
-    val={status:'file uploaded'}
-    render()
-    getres(res.image.url)}).catch(err=>val={text:'An unexpected error occured please try again',confidence:'100%'})
+  
+    getres(file)
 
   
 }
